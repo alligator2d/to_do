af60cb8658a4b8dc4a790cf51030226447fc5b66
Oleg <alligator2d@rambler.ru>
2022-12-22 10:04:06 +0300

    third commit

--- script.js
+++ script.js
@@ -30,8 +30,56 @@ function showList(obj) {
 	}
 }
 
-console.log(showList(list));
+// console.log(showList(list));
 changeStatus("write a post", "Done");
-console.log(showList(list));
+// console.log(showList(list));
 // addTask('have a walk');
 
+
+const films = [
+	{
+		name: 'Titanic',
+		rating: 9
+	},
+	{
+		name: 'Die hard 5',
+		rating: 5
+	},
+	{
+		name: 'Matrix',
+		rating: 8
+	},
+	{
+		name: 'Some bad film',
+		rating: 4
+	}
+];
+
+function showListOfFilms(arr) {
+	let temp = [];
+	 arr.forEach((item) => {
+		temp.push(item.name)
+	})
+	return temp.join(',');
+}
+console.log(showListOfFilms(films));
+
+
+
+const funds = [
+	{amount: -1400},
+	{amount: 2400},
+	{amount: -1000},
+	{amount: 500},
+	{amount: 10400},
+	{amount: -11400}
+];
+
+const getPositiveIncomeAmount = (data) => {
+	return data.filter(item => item.amount >0).reduce((acc,curr)=> acc + curr.amount,0)
+};
+console.log(getPositiveIncomeAmount(funds))
+const getTotalIncomeAmount = (data) => {
+	return data.filter(item => item.amount <0) ? data.reduce((acc, curr)=> acc + curr.amount,0) : getPositiveIncomeAmount(funds)
+};
+console.log(getTotalIncomeAmount(funds))
